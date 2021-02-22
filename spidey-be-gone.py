884c4cbf552f98f37fdb551755d752335754dc17
Luka Velinov <luka.velinov@gmail.com>
2021-02-22 22:15:47 +0100

    Added basic html parsing

--- spidey-be-gone.py
+++ spidey-be-gone.py
@@ -1,7 +1,23 @@
 import ebooklib
 from ebooklib import epub
 
+import bs4
+from bs4 import BeautifulSoup
+
 book = epub.read_epub('test/Itsy-Bitsy-Spider.epub')
 
 chapters = [item.get_content()
             for item in book.get_items_of_type(ebooklib.ITEM_DOCUMENT)]
+
+soup = BeautifulSoup(chapters[1], 'lxml')
+
+strings = [child for child in soup.descendants if type(
+    child) == bs4.element.NavigableString]
+
+
+def replace_creature(string):
+    pass
+
+
+for string in strings:
+    string.replace_with(replace_creature(string))
